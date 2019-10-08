d4301fa20f39fe59d63218deacea10935ba9f7d3
makskarp <46316793+makskarp@users.noreply.github.com>
2019-10-09 00:07:33 +0300

    All done but needs refactoring or writing from 0

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ dtt.onclick = function (event) {
         dtt.focus();
     }
 };
+const modal = document.getElementById("myModal");
+const modalEdit = document.getElementById("myModalEdit");
+const btnAddItemToList = document.getElementById("btnAddToList");
+const btnSortAllList = document.getElementById("btnSortAllList");
+const textSearch = document.getElementById("textSearch");
 
-let modal = document.getElementById("myModal");
-let modalEdit = document.getElementById("myModalEdit");
-let btn = document.getElementById("myBtn");
-let span = document.getElementsByClassName("close")[0];
-let spanEdit = document.getElementsByClassName("close")[1];
-let itemsStorageFlex = document.getElementById("itemsStorageFlex");
+const span = document.getElementsByClassName("close")[0];
+const spanEdit = document.getElementsByClassName("close")[1];
+const itemsStorageFlex = document.getElementById("itemsStorageFlex");
 
 
-btn.onclick = function() {
+btnAddItemToList.onclick = function() {
     modal.style.display = "block";
 };
 
@@ -35,13 +37,25 @@ window.onclick = function(event) {
         modalEdit.style.display = "none";
     }
 };
+
+textSearch.addEventListener("input", (event) => {
+    const {value} = event.target;
+    if(value.length){
+        showSearch(value)
+    }
+    else{
+        const divs = document.getElementsByClassName("listItems");
+        for (let i = divs.length - 1; i >= 0; i--) {
+            divs[i].remove();
+        }
+        load();
+    }
+} );
 const localStorageName = "objects";
-const station = "station";
 
 const resetLocalStorage = function (obj) {
     let oldObjs = JSON.parse(localStorage.getItem(localStorageName)) || [];
     oldObjs.push(obj);
-
     localStorage.setItem(localStorageName, JSON.stringify(oldObjs));
 };
 
@@ -51,10 +65,12 @@ const addInfoToLocalStorage = function(){
     const prior = document.getElementById("select");
     const deadline = document.getElementById("deadline");
     const obj = {
+        "id": Date.now(),
         "name": name.value,
         "desc": desc.value,
         "prior": prior.value,
-        "deadline": deadline.value
+        "deadline": deadline.value,
+        "done" : false,
     };
     resetLocalStorage(obj);
 };
@@ -63,18 +79,20 @@ const addInfoToLocalStorageEditMode = function () {
     const nameEdit = document.getElementById("nameEdit");
     const descEdit = document.getElementById("descriptionEdit");
     const priorEdit = document.getElementById("selectEdit");
-    const deadlineEdit = document.getElementById("deadline");
+    const deadlineEdit = document.getElementById("deadlineEdit");
     const indexToEdit = document.getElementById("indexToEdit");
+    let index = indexToEdit.value;
     const obj = {
+        "id": index,
         "name": nameEdit.value,
         "desc": descEdit.value,
         "prior": priorEdit.value,
-        "deadline": deadlineEdit.value
+        "deadline": deadlineEdit.value,
+        "done": false,
     };
-    let index = indexToEdit.value;
     let oldObjs = JSON.parse(localStorage.getItem(localStorageName));
-    let newObjs = oldObjs.map( (item,indx) => {
-        if(indx === index){
+    let newObjs = oldObjs.map( (item) => {
+        if(item["id"] === index){
             return obj;
         }
         return item;
@@ -85,65 +103,45 @@ const addInfoToLocalStorageEditMode = function () {
 
 const deleteFromLocalStorage = function (index) {
     let oldObjs = JSON.parse(localStorage.getItem(localStorageName)) || [];
-    let newObjs = oldObjs.filter( (item,indx) => {
-        let bool = indx === index ? false : true;
+    let newObjs = oldObjs.filter( (item) => {
+        let bool = item["id"] === index ? false : true;
         return bool;
     });
     localStorage.setItem(localStorageName, JSON.stringify(newObjs));
 };
 
 const mark = function (index,done) {
-    const stationObj = {
-        "index" : index,
-    };
     const itemsToChange = document.getElementsByClassName("listItems");
     const itemToChange = Array.prototype.filter.call(itemsToChange, function(itemsToChange){
         return (itemsToChange.id === `${index}`);
     });
     let pTagsToChange = itemToChange[0].getElementsByTagName("p");
-    console.log(pTagsToChange);
     for(let item of pTagsToChange){
         if(done === true){
             item.style.cssText += "text-decoration: line-through";
-            stationObj["decoration"] = "text-decoration: line-through";
         }
         else{
             item.style.cssText += "text-decoration: none";
-            stationObj["decoration"] = "text-decoration: none";
         }
-
     }
     const buttonToChange = document.getElementById(`marker${index}`);
     if(done === true){
         buttonToChange.textContent = "Mark as undone";
-        stationObj["button"] = "Mark as undone";
         const editButtonToDelete = document.getElementById(`edit${index}`);
         editButtonToDelete.style.display = "none";
     }
     else{
         buttonToChange.textContent = "Mark as done";
-        stationObj["button"] = "Mark as done";
         const editButtonToDelete = document.getElementById(`edit${index}`);
         editButtonToDelete.style.display = "inline-block";
     }
-    let stationLocalStorage = JSON.parse(localStorage.getItem(station)) || [];
-    if(stationLocalStorage.length === 0){
-        stationLocalStorage.push(stationObj);
-    }
-    else{
-        let changedObj = false;
-        stationLocalStorage.forEach( (item) => {
-            if(item["index"] === index){
-                item["decoration"] = stationObj["decoration"];
-                item["button"] = stationObj["button"];
-                changedObj = true;
-            }
-        });
-        if(!changedObj){
-            stationLocalStorage.push(stationObj);
+    let stationLocalStorage = JSON.parse(localStorage.getItem(localStorageName)) || [];
+    stationLocalStorage.forEach( (item) => {
+        if(item["id"] === index){
+            done === true ? item["done"] = true : item["done"] = false;
         }
-    }
-    localStorage.setItem(station, JSON.stringify(stationLocalStorage));
+    });
+    localStorage.setItem(localStorageName, JSON.stringify(stationLocalStorage));
 };
 
 const markAsDone = function (index) {
@@ -154,13 +152,28 @@ const markAsUndone = function (index) {
     mark(index,false);
 };
 
-const load = function () {
+const showSearch = function (value) {
     let objInLocStorage = JSON.parse(localStorage.getItem(localStorageName)) || [];
     if(objInLocStorage.length === 0){
+        alert("No items");
         return null;
     }
     else{
-        objInLocStorage.forEach( (item,index) =>{
+        const divs = document.getElementsByClassName("listItems");
+        for (let i = divs.length - 1; i >= 0; i--) {
+            divs[i].remove();
+        }
+        const nameFilltered = objInLocStorage.filter( (item) => {
+           return item["name"].includes(value);
+        });
+        const descFilltered = objInLocStorage.filter( (item) => {
+            return item["desc"].includes(value);
+        });
+        const matches = nameFilltered.concat(descFilltered);
+        const result = matches.filter( (item,indx) => {
+            return matches.indexOf(item) === indx;
+        });
+        result.forEach( (item) =>{
             let itemDiv = document.createElement("div");
             let itemName = item["name"];
             let itemDesc = item["desc"];
@@ -170,7 +183,7 @@ const load = function () {
             }
             itemDiv.classList.add("listItems");
             itemDiv.style.width = "19%";
-            itemDiv.setAttribute("id",index + "");
+            itemDiv.setAttribute("id",item["id"] + "");
             let itemButtons = document.createElement("div");
             itemButtons.classList.add("listItemsButtons");
             let arrPTags = [];
@@ -189,11 +202,11 @@ const load = function () {
             arrButtons[0].textContent = "Delete";
             arrButtons[0].style.cssText += "background-color: rgb(243, 65, 11);";
             arrButtons[0].onclick = function(){
-              deleteFromLocalStorage(index);
-              location.reload();
+                deleteFromLocalStorage(item["id"]);
+                location.reload();
             };
             arrButtons[1].textContent = "Edit";
-            arrButtons[1].id = `edit${index}`;
+            arrButtons[1].id = `edit${item["id"]}`;
             arrButtons[1].style.cssText += "background-color: #4CAF50;";
             arrButtons[1].onclick = function(){
                 modalEdit.style.display = "block";
@@ -202,21 +215,21 @@ const load = function () {
                 const prior = document.getElementById("selectEdit");
                 const deadline = document.getElementById("deadlineEdit");
                 const indexToEdit = document.getElementById("indexToEdit");
-                indexToEdit.value = index;
+                indexToEdit.value = item["id"];
                 name.value = item.name;
                 desc.value = item.desc;
                 prior.value = item.prior;
                 deadline.value = item.deadline;
             };
             arrButtons[2].style.cssText += "background-color: #254266;";
-            arrButtons[2].id = `marker${index}`;
+            arrButtons[2].id = `marker${item["id"]}`;
             arrButtons[2].textContent = "Mark as done";
             arrButtons[2].onclick = function(){
                 if(arrButtons[2].textContent === "Mark as done") {
-                    markAsDone(index);
+                    markAsDone(item["id"]);
                 }
                 else{
-                    markAsUndone(index);
+                    markAsUndone(item["id"]);
                 }
             };
             arrButtons.forEach((elem) => {
@@ -236,25 +249,148 @@ const load = function () {
             });
             itemDiv.appendChild(itemButtons);
             itemsStorageFlex.insertAdjacentElement("afterbegin", itemDiv);
-            let stationLocalStorage = JSON.parse(localStorage.getItem(station)) || [];
-            if(stationLocalStorage.length !== 0){
-                stationLocalStorage.forEach( (item) => {
-                        if(item["index"] === index){
-                            arrPTags.forEach( (tag) => {
-                                tag.style.cssText += item["decoration"];
-                            });
-                            arrButtons[2].textContent = item["button"];
-                            if(arrButtons[2].textContent === "Mark as undone"){
-                                const editButtonToDelete = document.getElementById(`edit${index}`);
-                                editButtonToDelete.style.display = "none";
-                            }
-                        }
-                    }
-                );
+            if(item["done"] === true) {
+                arrPTags.forEach((tag) => {
+                    tag.style.cssText += "text-decoration: line-through;";
+                });
+                arrButtons[2].textContent = "Mark as undone";
+                if (arrButtons[2].textContent === "Mark as undone") {
+                    const editButtonToDelete = document.getElementById(`edit${item["id"]}`);
+                    editButtonToDelete.style.display = "none";
+                }
             }
-            localStorage.setItem(station, JSON.stringify(stationLocalStorage));
-    })
+        });
     }
+
+};
+
+const sortList = function () {
+    let objInLocStorage = JSON.parse(localStorage.getItem(localStorageName)) || [];
+    objInLocStorage.sort( (a,b) => {
+        if(a["done"] === true && b["done"] === false) {
+            return -1;
+        }
+        else if(a["done"] === false && b["done"] === true) {return 1;}
+        else {
+            let priorA = a["prior"];
+            let priorB = b["prior"];
+            let compareObj = {
+                "Low" : 0,
+                "Medium" : 1,
+                "High" : 2,
+            };
+            if(compareObj[priorA] > compareObj[priorB]){
+                return 1;
+            }
+            else if(compareObj[priorA] === compareObj[priorB]){
+                return 0;
+            }
+            else{
+                return -1;
+            }
+        }
+    });
+    localStorage.setItem(localStorageName, JSON.stringify(objInLocStorage));
+    location.reload();
+};
+const load = function () {
+    let objInLocStorage = JSON.parse(localStorage.getItem(localStorageName)) || [];
+    if(objInLocStorage.length === 0){
+        return null;
+    }
+    else{
+            objInLocStorage.forEach( (item) =>{
+                let itemDiv = document.createElement("div");
+                let itemName = item["name"];
+                let itemDesc = item["desc"];
+                let itemPrior = item["prior"];
+                if(itemDesc.length === 0){
+                    itemDesc = "No description";
+                }
+                itemDiv.classList.add("listItems");
+                itemDiv.style.width = "19%";
+                itemDiv.setAttribute("id",item["id"] + "");
+                let itemButtons = document.createElement("div");
+                itemButtons.classList.add("listItemsButtons");
+                let arrPTags = [];
+                let arrButtons = [];
+                let arrClassesForP = ["listItemsName","listItemsDescription","listItemsPriority"];
+                for(let i = 0; i < 3; i++){
+                    arrPTags.push(document.createElement("p"));
+                    arrPTags[i].classList.add(arrClassesForP[i]);
+                    arrButtons.push(document.createElement("button"));
+                    arrButtons[i].type = "button";
+                    arrButtons[i].style.cssText = `border: none;color: white; margin-right: 10px;padding: 4px 14px ;text-align: center;font-size: 14px;`;
+                }
+                arrPTags[0].textContent = itemName;
+                arrPTags[1].textContent = itemDesc;
+                arrPTags[2].textContent = itemPrior;
+                arrButtons[0].textContent = "Delete";
+                arrButtons[0].style.cssText += "background-color: rgb(243, 65, 11);";
+                arrButtons[0].onclick = function(){
+                  deleteFromLocalStorage(item["id"]);
+                  location.reload();
+                };
+                arrButtons[1].textContent = "Edit";
+                arrButtons[1].id = `edit${item["id"]}`;
+                arrButtons[1].style.cssText += "background-color: #4CAF50;";
+                arrButtons[1].onclick = function(){
+                    modalEdit.style.display = "block";
+                    const name = document.getElementById("nameEdit");
+                    const desc = document.getElementById("descriptionEdit");
+                    const prior = document.getElementById("selectEdit");
+                    const deadline = document.getElementById("deadlineEdit");
+                    const indexToEdit = document.getElementById("indexToEdit");
+                    indexToEdit.value = item["id"];
+                    name.value = item.name;
+                    desc.value = item.desc;
+                    prior.value = item.prior;
+                    deadline.value = item.deadline;
+                };
+                arrButtons[2].style.cssText += "background-color: #254266;";
+                arrButtons[2].id = `marker${item["id"]}`;
+                arrButtons[2].textContent = "Mark as done";
+                arrButtons[2].onclick = function(){
+                    if(arrButtons[2].textContent === "Mark as done") {
+                        markAsDone(item["id"]);
+                    }
+                    else{
+                        markAsUndone(item["id"]);
+                    }
+                };
+                arrButtons.forEach((elem) => {
+                    itemButtons.appendChild(elem);
+                });
+                if(item["prior"] === 'High'){
+                    arrPTags[2].style.color = "red";
+                }
+                else if(item["prior"] === "Medium"){
+                    arrPTags[2].style.color = "yellow";
+                }
+                else{
+                    arrPTags[2].style.color = "blue ";
+                }
+                arrPTags.forEach( (elem) => {
+                    itemDiv.appendChild(elem);
+                });
+                itemDiv.appendChild(itemButtons);
+                itemsStorageFlex.insertAdjacentElement("afterbegin", itemDiv);
+                if(item["done"] === true) {
+                    arrPTags.forEach((tag) => {
+                        tag.style.cssText += "text-decoration: line-through;";
+                    });
+                    arrButtons[2].textContent = "Mark as undone";
+                    if (arrButtons[2].textContent === "Mark as undone") {
+                        const editButtonToDelete = document.getElementById(`edit${item["id"]}`);
+                        editButtonToDelete.style.display = "none";
+                    }
+                }
+    });
+    }
+};
+
+btnSortAllList.onclick = function(){
+    sortList();
 };
 
 window.addEventListener('load',load(),false);
